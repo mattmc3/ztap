e0f275fbbf74e3ae644607dbb893a5ff2955aeff
mattmc3 <mattmc3@gmail.com>
2021-01-13 22:49:16 -0500

    More tests

--- tests/files.zsh
+++ tests/files.zsh
@@ -6,7 +6,9 @@ builtin cd $temp
 
 @test "an existing thing on the filesystem" -e $temp
 @test "an existing directory" -d $temp
+@test "a non-existing directory" ! -d "${temp}.fake"
 @test "a regular file" $(command touch file) -f file
+@test "a non-existing regular file" ! -f fake
 @test "the file is empty" -z $(read <file)
 
 command rm -rf $temp
